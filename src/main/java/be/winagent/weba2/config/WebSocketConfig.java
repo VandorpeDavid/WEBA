d23215540acadc1a6485b9c55a97577933ef0ced
David Vandorpe <david.vandorpe2@gmail.com>
2021-06-24 20:08:43 +0200

    Im not desperate, you are.

--- src/main/java/be/winagent/weba2/config/WebSocketConfig.java
+++ src/main/java/be/winagent/weba2/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websockets").withSockJS();
+        registry.addEndpoint("/websockets").setAllowedOrigins("*").withSockJS();
     }
 }
